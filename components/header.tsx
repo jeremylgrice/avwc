a3bc4d743edced2f8cd618211c2936685ff0a964
Jeremy <Jeremylgrice@gmail.com>
2022-10-15 11:01:25 -0400

    address issue with header

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <h2 className="text-7xl md:text-1xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/">
-        <a className="hover:underline">Vet That Codes</a>
-      </Link>
-    </h2>
+    <section className="flex-col md:flex-row flex items-center md:justify-between">
+      <h2 className="text-5xl md:text-7xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8"> 
+        <Link href="/">
+          <a className="hover:underline">Vet That Codes</a>
+        </Link>
+      </h2>
+    </section>
   )
 }
 
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,12 +3,9 @@ import { CMS_NAME } from '../lib/constants'
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mb-20 mt-8 md:mb-12">
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h2 className="text-5xl md:text-7xl font-bold tracking-tight md:tracking-tighter leading-tight">
         Vet That Codes
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        
-      </h4>
+      </h2>
     </section>
   )
 }
